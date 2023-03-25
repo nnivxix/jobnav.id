ab1abeca0dcc783c7c7f70dfab37301d180006b4
nnivxix <adhe.hanasa@gmail.com>
2023-03-25 15:51:23 +0700

    add config color and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./resources/**/*.js",
     "./resources/**/*.vue",
   ],
+  colors: {
+    "carrot-10": "#F9DFCF",
+    "carrot-600": "#E9803E",
+    "grass-500": "#28831E",
+    "smoke-700": "#263238",
+    "smoke-400": "#4B4B4B",
+    "smoke-800": "#30343F",
+  },
   theme: {
     extend: {},
   },
