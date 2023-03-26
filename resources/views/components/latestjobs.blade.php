8bc8a6b00b12f544d87a932f3a0e47b67fcceca7
nnivxix <adhe.hanasa@gmail.com>
2023-03-26 17:37:15 +0700

    fix import module

--- resources/views/components/latestjobs.blade.php
+++ resources/views/components/latestjobs.blade.php
@@ -8,7 +8,9 @@
       <img class="col-start-1 place-self-center p-3 col-end-2 row-start-1 row-end-3" src="{{ $job['icon'] }}" alt="{{ $job['company'] }}" srcset="">
       <h1 class="text-smoke-700 col-start-2 col-end-5" id="company">{{ $job['company'] }}</h1>
       <h2 class="text-smoke-700 row-start-2 col-start-2 col-end-5" id="position">{{ $job['position'] }}</h2>
-      <button class="col-start-6 row-span-2 hover:bg-carrot-600 hover:text-white rounded-md">Save</button>
+      <button class="col-start-6 row-span-2 rounded-md flex justify-center items-center">
+        <img src="{{ URL::asset('images/bookmark.svg') }}" alt="bookmark" title="bookmark icon">
+      </button>
     </div>
     <h3 class="py-4 font-bold text-xl text-smoke-800" id="job title">{{ $job['title'] }}</h3>
     <p class="text-smoke-400 text-md">{{ $job['description'] }}</p>
