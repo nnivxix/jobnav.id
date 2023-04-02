58830ed9b32d5685b936a125258fae83599a0644
nnivxix <adhe.hanasa@gmail.com>
2023-04-02 13:29:30 +0700

    fix padding footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
   <a class="col-start-1 col-end-1 text-4xl font-bold py-6 mt-8" href="#">Jobnav.id</a>
   <a class="col-start-1 col-end-1 text-sm" href="emailto:email@jobnav.id">email@jobnav.id</a>
   <div class="col-start-1 col-end-1 flex  py-4">
-    <img class="px-4" src="{{ URL::asset('images/ic_discord.svg') }}" alt="" srcset="">
+    <img class="pr-4" src="{{ URL::asset('images/ic_discord.svg') }}" alt="" srcset="">
     <img class="px-4" src="{{ URL::asset('images/ic_facebook.svg') }}" alt="" srcset="">
     <img class="px-4" src="{{ URL::asset('images/ic_linkedin.svg') }}" alt="" srcset="">
     <img class="px-4" src="{{ URL::asset('images/ic_twitter.svg') }}" alt="" srcset="">
