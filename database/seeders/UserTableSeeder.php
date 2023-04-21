add992403386e83086dded10dec1e5b9bea61cde
nnivxix <adhe.hanasa@gmail.com>
2023-04-21 10:05:11 +0700

    feat: user seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $hanasa = User::firstOrCreate([
+      'name'              => 'Hanasa',
+      'username'          => 'hanasa',
+      'email'             => 'hanasa@hanasa.com',
+      'email_verified_at' => now(),
+    ], [
+      'password' => bcrypt('password'),
+    ]);
+  }
+}
