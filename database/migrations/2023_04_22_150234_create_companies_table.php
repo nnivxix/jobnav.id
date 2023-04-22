2666c7a506e8d2b847ccf2bf00917af5397c6a9f
nnivxix <adhe.hanasa@gmail.com>
2023-04-22 22:12:55 +0700

    feat: make migration

---
+++ database/migrations/2023_04_22_150234_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('avatar');
+            $table->string('image_cover');
+            $table->text('about');
+            $table->string('ownedby');
+            $table->string('location');
+            $table->string('full_addres');
+            $table->string('website');
+            $table->date('posted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
