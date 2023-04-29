95ad96b37f6bb1476134d8bacca4bd1d7a2f07a4
nnivxix <adhe.hanasa@gmail.com>
2023-04-29 19:57:01 +0700

    fix: aligment

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -18,15 +18,15 @@ class JobFactory extends Factory
     public function definition()
     {
         return [
-            'uuid' => fake()->uuid(),
-            'company_id' => fn () => Company::factory()->create()->id,
-            'title' => fake()->jobTitle(),
-            'position' => fake()->sentence(3, false),
-            'location' => fake()->country(),
-            'salary' => fake()->numberBetween(200, 10000),
+            'uuid'        => fake()->uuid(),
+            'company_id'  => fn () => Company::factory()->create()->id,
+            'title'       => fake()->jobTitle(),
+            'position'    => fake()->sentence(3, false),
+            'location'    => fake()->country(),
+            'salary'      => fake()->numberBetween(200, 10000),
             'description' => fake()->sentence(56, false),
-            'categories' => str_replace(" ", ",", fake()->sentence(7)),
-            'posted_at' => fake()->dateTimeInInterval('now', '+1 year'),
+            'categories'  => str_replace(" ", ",", fake()->sentence(7)),
+            'posted_at'   => fake()->dateTimeInInterval('now', '+1 year'),
         ];
     }
 }
