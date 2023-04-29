a69811efbf62e296629ccc1a01b706b847a2538b
nnivxix <adhe.hanasa@gmail.com>
2023-04-29 19:02:15 +0700

    feat: create job migration

---
+++ database/migrations/2023_04_29_115020_create_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('company_id');
+            $table->string('title');
+            $table->string('position');
+            $table->string('location');
+            $table->integer('salary');
+            $table->longText('description');
+            $table->date('posted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
