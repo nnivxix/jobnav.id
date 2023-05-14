d427992c9147b7fadbedeaa204224da29c98e60a
nnivxix <adhe.hanasa@gmail.com>
2023-05-14 18:21:04 +0700

    feat: add button apply

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -26,6 +26,9 @@
         @endforeach
         <h1 class="text-2xl mt-6 font-bold"></h1>
         <p class="text-lg text-justify pr-5">{{$job['description']}}</p>
+        <form class="my-4">
+            <button class="bg-carrot-600 text-white p-3">Apply Now</button>
+        </form>
     </div>
     <div class="col-span-2">
         <h1 class="text-xl font-medium  text-smoke-700 py-3">Field Job in <span class="font-bold"> {{$job->company->name}}</span></h1>
